ed19408a3b62189b7bad0e525845ee2c3c0b0f61
Oli Davis <oli.davis@me.com>
2012-11-14 14:08:52 +0000

    Initial Code injection (Mac compileable)

---
+++ source/main.cpp
@@ -0,0 +1,46 @@
+//
+//  main.cpp
+//  OOD Assignment Sample
+//
+//  Created by Oli Davis on 09/11/2012.
+//  Copyright (c) 2012 Oli Davis. All rights reserved.
+//
+
+#include <iostream>
+
+// assign1011.cpp : Defines the entry point for the console application.
+//
+
+#include "window.h"
+
+void draw(void)
+{
+	glBegin(GL_LINE_LOOP);								// Draw the 3 connected verticies of a triangle
+    glVertex3f( 0.0f, 1.0f, 0.0f);					// Top
+    glVertex3f(-1.0f,-1.0f, 0.0f);					// Bottom Left
+    glVertex3f( 1.0f,-1.0f, 0.0f);					// Bottom Right
+	glEnd();											// Finished Drawing The Triangle
+    
+	glTranslatef(-2.5f,0.0f,0.0f);						// Move Left 1.5 Units
+	glRotatef(20,0,0,1);								// Rotate by 20 degrees
+	glBegin(GL_LINE_LOOP);								// Draw the triangle again
+    glVertex3f( 0.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f,-1.0f, 0.0f);
+    glVertex3f( 1.0f,-1.0f, 0.0f);
+	glEnd();
+    
+	glRotatef(-20,0,0,1);								// Rotate back again
+	glTranslatef(5.0f,0.0f,0.0f);						// Move Right 5 Units
+	glBegin(GL_LINE_LOOP);								// Draw a square
+    glVertex3f(-1.0f, 1.0f, 0.0f);					// Top Left
+    glVertex3f( 1.0f, 1.0f, 0.0f);					// Top Right
+    glVertex3f( 1.0f,-1.0f, 0.0f);					// Bottom Right
+    glVertex3f(-1.0f,-1.0f, 0.0f);					// Bottom Left
+	glEnd();
+}
+
+int main ( int argc, char** argv )   // Create Main Function For Bringing It All Together
+{
+	window w(argc,argv);
+}
+
---
+++ source/window.h
@@ -0,0 +1,76 @@
+#ifndef __WINDOW_H__
+#define __WINDOW_H__
+
+//#include <GL/gl.h>     // The GL Header File
+//#include <GL/glut.h>   // The GL Utility Toolkit (Glut) Header
+#include <GLUT/glut.h>
+#include <stdlib.h>
+
+static void draw(void);
+
+class window {
+public:
+	window(int argc, char** argv);
+	~window(){};
+    
+	static void reshape(int w,int h);
+	static void keyboard ( unsigned char key, int x, int y );
+	static void display();
+    
+};
+
+window::window(int argc, char** argv)
+{
+	glutInit( &argc, argv );
+	glutInitWindowSize  ( 500, 500 );
+	glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE );
+	glutCreateWindow    ( "ODD assignment" );
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glEnable(GL_COLOR_MATERIAL );
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glutReshapeFunc     ( reshape );
+	glutDisplayFunc     ( display );
+	glutKeyboardFunc    ( keyboard );
+	glutMainLoop        ( );
+}
+
+void window::reshape ( int w, int h )
+{
+    glViewport     ( 0, 0, w, h );
+    glMatrixMode   ( GL_PROJECTION );
+    glLoadIdentity ( );
+    if ( h==0 )
+        gluPerspective ( 80, ( float ) w, 1.0, 5000.0 );
+    else
+        gluPerspective ( 80, ( float ) w / ( float ) h, 1.0, 5000.0 );
+    glMatrixMode   ( GL_MODELVIEW );
+    glLoadIdentity ( );
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+}
+
+void window::keyboard( unsigned char key, int x, int y )
+{
+    switch ( key ) {
+        case 27:
+            exit ( 0 );
+            break;
+        default:
+            break;
+    }
+}
+
+void window::display()
+{
+	glLoadIdentity();
+	glTranslatef(0.0f,0.0f,-6.0f);
+	glColor3f(1,0,0);
+	draw();
+	glutSwapBuffers ( );
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+#endif /* __WINDOW_H__ */
