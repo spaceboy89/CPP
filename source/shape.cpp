402806be4da9ba24a165ee063760484b07e0a9a8
Oli Davis <oli.davis@me.com>
2012-11-27 21:57:39 +0000

    Modified example draw code to be example for our program

--- source/shape.cpp
+++ source/shape.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  shape.cpp
 //  OOD Assignment Sample
 //
 //  Created by Oli Davis on 09/11/2012.
@@ -7,38 +7,37 @@
 //
 
 #include <iostream>
-
-// assign1011.cpp : Defines the entry point for the console application.
-//
-
 #include "window.h"
 //#include "Instruction.h"
 //#include "ParseInput.h"
 
 void draw(void)
 {
-	glBegin(GL_LINE_LOOP);								// Draw the 3 connected verticies of a triangle
-    glVertex3f( 0.0f, 1.0f, 0.0f);					// Top
-    glVertex3f(-1.0f,-1.0f, 0.0f);					// Bottom Left
-    glVertex3f( 1.0f,-1.0f, 0.0f);					// Bottom Right
-	glEnd();											// Finished Drawing The Triangle
-    
-	glTranslatef(-2.5f,0.0f,0.0f);						// Move Left 1.5 Units
-	glRotatef(20,0,0,1);								// Rotate by 20 degrees
-	glBegin(GL_LINE_LOOP);								// Draw the triangle again
-    glVertex3f( 0.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f,-1.0f, 0.0f);
-    glVertex3f( 1.0f,-1.0f, 0.0f);
+    // Start drawing a line
+    glBegin(GL_LINE_STRIP);			
+        glVertex3f( 0.0f, 0.0f, 0.0f);
+        glVertex3f(0.0f,1.0f, 0.0f);
 	glEnd();
+    glTranslatef(0.0, 1.0, 0);  // Move cursor to end of line
+    // End the drawing of a line
     
-	glRotatef(-20,0,0,1);								// Rotate back again
-	glTranslatef(5.0f,0.0f,0.0f);						// Move Right 5 Units
-	glBegin(GL_LINE_LOOP);								// Draw a square
-    glVertex3f(-1.0f, 1.0f, 0.0f);					// Top Left
-    glVertex3f( 1.0f, 1.0f, 0.0f);					// Top Right
-    glVertex3f( 1.0f,-1.0f, 0.0f);					// Bottom Right
-    glVertex3f(-1.0f,-1.0f, 0.0f);					// Bottom Left
+    // rotate cursor 90 degrees clockwise
+    glRotatef(-90, 0, 0, 1);
+	// end rotate
+    
+    glBegin(GL_LINE_STRIP);
+        glVertex3f(0.0f,0.0f, 0.0f);
+        glVertex3f( 0.0f,1.0f, 0.0f);
 	glEnd();
+    glTranslatef(0.0, 1.0, 0);
+    
+    glRotatef(45, 0, 0, 1);
+    
+	glBegin(GL_LINE_STRIP);
+        glVertex3f(0.0f,0.0f, 0.0f);
+        glVertex3f( 0.0f,1.0f, 0.0f);
+	glEnd();
+    glTranslatef(0.0, 1.0, 0);
 }
 
 int main ( int argc, char** argv )   // Create Main Function For Bringing It All Together
